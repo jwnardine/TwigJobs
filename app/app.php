6b96f16f51311a4e7f641443527411565eb8b734
jon nardine and matt knutson <jwnardine@gmail.com>
2016-02-10 13:39:12 -0800

    finish jobs form

--- app/app.php
+++ app/app.php
@@ -1,11 +1,11 @@
 <?php
     require_once __DIR__."/../vendor/autoload.php";
-    require_once __DIR__."/../src/Places.php";
+    require_once __DIR__."/../src/Jobs.php";
 
     session_start();
 
-    if (empty($_SESSION['list_of_places'])) {
-            $_SESSION['list_of_places'] = array();
+    if (empty($_SESSION['list_of_jobs'])) {
+            $_SESSION['list_of_jobs'] = array();
         }
 
     $app = new Silex\Application();
@@ -16,19 +16,19 @@
 
     $app->get("/", function() use ($app) {
 
-        return $app['twig']->render('places.html.twig', array('places' => Place::getAll()));
+        return $app['twig']->render('jobs.html.twig', array('jobs' => Jobs::getAll()));
 
       });
 
-      $app->post("/places", function() use ($app) {
-          $city = new Place($_POST['city'], $_POST['picture']);
-          $city->save();
-          return $app['twig']->render('create_places.html.twig', array('newcity' => $city));
+      $app->post("/jobs", function() use ($app) {
+          $jobs = new Jobs($_POST['title'], $_POST['salary'], $_POST['desc']);
+          $jobs->save();
+          return $app['twig']->render('create_jobs.html.twig', array('newjob' => $jobs));
       });
 
-          $app->post("/delete_places", function() use ($app) {
-          Place::deleteAll();
-          return $app['twig']->render('delete_places.html.twig');
+          $app->post("/delete_jobs", function() use ($app) {
+          Jobs::deleteAll();
+          return $app['twig']->render('delete_jobs.html.twig');
       });
 
     return $app;
